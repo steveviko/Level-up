ac99a463a15d689e6922f7ee2fd2717bc0fedd80
steveviko <stevenviko@gmail.com>
2018-08-08 17:27:53 +0300

    signUp app updates

--- UI/app.py
+++ UI/app.py
@@ -1,13 +1,22 @@
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+class SignUp(object):
+    """A user of uber fills in his/her credentails to signup for an account """
+
+    def __init__(self, username,email, password):
+        """Return a user object with the following properties ."""
+        self.username = username       
+        self.email = email
+        self.password = password
+
+    def Fullname(self):
+        pass
+
+
+    def submit(self):
+        pass
+        
+    @staticmethod
+    def validate_email(self):
+        pass
+
+
+    

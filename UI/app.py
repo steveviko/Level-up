d9b55f616a2b17588b200e37184bef0d9207f27d
steveviko <stevenviko@gmail.com>
2018-08-09 11:44:09 +0300

    updates on signup app

--- UI/app.py
+++ UI/app.py
@@ -1,22 +1,34 @@
-class SignUp(object):
-    """A user of uber fills in his/her credentails to signup for an account """
+import re
+class Signup():
+    def __init__(self, FirstName, LastName, email_addr):
+        self.FirstName = str(FirstName)
+        self.LastName = str(LastName)
+        self.email_addr = Signup.validate_email(email_addr)
+        self.User_db= []
 
-    def __init__(self, username,email, password):
-        """Return a user object with the following properties ."""
-        self.username = username       
-        self.email = email
-        self.password = password
-
-    def Fullname(self):
-        pass
-
+    def Full_Names(self):
+        if not (self.FirstName and self.LastName):
+            return {'response':'please enter your first name and last name '}
+        else:
+            return {self.FirstName+" " + self.LastName}
 
     def submit(self):
-        pass
-        
+        if not (self.FirstName and self.LastName):
+            return {'response':'please enter your first name and last name '}
+        else:
+            self.users = {'FirstName': self.FirstName,
+                        'LastName': self.LastName, 'email': self.email_addr}
+            self.User_db.append(self.users)
+            return self.User_db
+
+
     @staticmethod
-    def validate_email(self):
-        pass
+    def validate_email(email):
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            return False
+        else:
+            return email
+
 
 
     
